1de0faf57404552b47e8ea31ac2bf8d3d79594c5
Quoc Anh Dang <vincentdang92@gmail.com>
2021-11-02 13:51:43 +0700

    config ssl

---
+++ pages/login/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Login
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- plugins/RangeSlider.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
-Vue.use(VueCompositionAPI)
-import Slider from '@vueform/slider/dist/slider.vue2.js'
-Vue.use(Slider) 
